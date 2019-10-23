379e2577ab8ee1fc656c3947a0e4d4cf9c4b8417
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:57:56 -0700

    Framework extraction update
    Framework extraction update

---
+++ Interfaces/IPenguinMiddleware.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Penguin.Web.Abstractions.Interfaces
+{
+    public interface IPenguinMiddleware
+    {
+        Task Invoke(HttpContext context);
+    }
+}
---
+++ Interfaces/IRouteConfig.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Penguin.Web.Abstractions.Interfaces
+{
+    public interface IRouteConfig
+    {
+        void RegisterRoutes(IRouteBuilder routes);
+    }
+}
